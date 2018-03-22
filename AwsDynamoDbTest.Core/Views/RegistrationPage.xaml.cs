073d405dbe45dd03e81f2c204457b3d9228c3b8e
fritzlim <fritzlim@gmail.com>
2018-03-23 01:25:48 +0800

    [WIP] Added commanding to call RegisterPerson() WIP because the command doesn't fire.

--- AwsDynamoDbTest.Core/Views/RegistrationPage.xaml.cs
+++ AwsDynamoDbTest.Core/Views/RegistrationPage.xaml.cs
@@ -1,20 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
+﻿//using System;
+//using System.Collections.Generic;
+using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using AwsDynamoDbTest.Core.Helpers;
 
 namespace AwsDynamoDbTest.Core.Views
 {
     public partial class RegistrationPage : ContentPage
     {
+
+
         public RegistrationPage()
         {
             InitializeComponent();
         }
 
+        async Task RegisterPerson()
+        {
+            await AwsDynamoDbHelper.Instance().SaveItemAsync("Called SaveItemAsync() from RegisterPerson()");
+        }
+
         async void Handle_Clicked(object sender, System.EventArgs e)
         {
-            
+            await AwsDynamoDbHelper.Instance().SaveItemAsync("Called SaveItemAsync() from Handle_Clicked()");
         }
     }
 }
