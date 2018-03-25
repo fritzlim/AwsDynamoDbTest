a99098d4705326e5e9c814fc6623fb3a685860dd
fritzlim <fritzlim@gmail.com>
2018-03-25 11:23:17 +0800

    Code cleanup for RegistrationPage.xaml.cs Removed extra carriage returns.

--- AwsDynamoDbTest.Core/Views/RegistrationPage.xaml.cs
+++ AwsDynamoDbTest.Core/Views/RegistrationPage.xaml.cs
@@ -9,8 +9,6 @@ namespace AwsDynamoDbTest.Core.Views
 {
     public partial class RegistrationPage : ContentPage
     {
-
-
         public RegistrationPage()
         {
             InitializeComponent();
