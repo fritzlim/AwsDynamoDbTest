4cb23510d9faa26c65223ee084d63c1cd62759e2
fritzlim <fritzlim@gmail.com>
2018-04-09 00:41:31 +0800

    [WIP] Added RetrievePage to demo querying the DynamoDB table Also added explanation text to RegisterPage and RetrevePage.
    WIP because the RetrievePage isn't complete. And it's view model needs to be created.

---
+++ AwsDynamoDbTest.Core/Views/RetrievePage.xaml.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+//using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using AwsDynamoDbTest.Core.Helpers;
+
+namespace AwsDynamoDbTest.Core.Views
+{
+    public partial class RetrievePage : ContentPage
+    {
+        public RetrievePage()
+        {
+            InitializeComponent();
+        }
+
+        async Task RegisterPerson()
+        {
+            await AwsDynamoDbHelper.Instance().SaveItemUsingGivenNameAsync("Called SaveItemAsync() from RegisterPerson()");
+        }
+
+        async void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            await AwsDynamoDbHelper.Instance().SaveItemUsingGivenNameAsync("Called SaveItemAsync() from Handle_Clicked(). userName=" + userName.Text);
+        }
+    }
+}
